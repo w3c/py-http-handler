8e2a5b2d1d2ef34493f8aa8d735c3ace166cb4a7
Jose Kahan <jkbzh@w3.org>
2024-08-06 23:04:44 +0200

    be explicit re requirement to trim space off cookies until we find a way to avoid it

--- credentialfilter_ex.bash
+++ credentialfilter_ex.bash
@@ -5,13 +5,17 @@
 #
 
 # should be read from HTTP_COOKIES or any other dedicated env variable
-COOKIES="login=abc;other_cookie=87A9Er;cloudflare_bot=1305EFA13CB"
+COOKIES="login=abc; other_cookie=87A9Er; cloudflare_bot=1305EFA13CB"
 TARGET_URL="https://www.example.org/ab/cd/de?q=r2+y=24"
 CONFIG_FILE="/tmp/py-http-handler/credentialfilter.conf.dist"
 
-RESULTX="$(./credentialfilter -cf ${CONFIG_FILE} \
-			      -u  $TARGET_URL \
-			      -c ${COOKIES}; echo x$?)"
+# due to limitations of bash (or my lack of experience) we have
+# to remove all white space in the cookie
+trimmed_cookies=${COOKIES//[[:blank:]]/}
+
+RESULTX=$"$(./credentialfilter -cf ${CONFIG_FILE} \
+			      -u  ${TARGET_URL} \
+			      -c ${trimmed_cookies}; echo x$?)"
 
 TARGET_HOST_TRUST_LEVEL=${RESULTX##*x}
 FILTERED_COOKIES="${RESULTX%x*}"
@@ -21,7 +25,7 @@ echo "#filtered cookies, output as HTTP Set-Cookie lines"
 
 IFS=$'\n' readarray -t cookie_arr <<<"${FILTERED_COOKIES}"
 
-# drop the extra \n the script outpus in the last line
+# drop the extra \n the script outputs in the last line
 unset cookie_arr[-1]
 
 for i in "${cookie_arr[@]}"
